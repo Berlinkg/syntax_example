e9c0af9dc8a799c5d09d67e24d8035e91eb21fdf
Berlinkg <nadyrbekov570@gmail.com>
2023-04-17 11:08:44 +0600

    syntax

---
+++ bin/second.dart
@@ -0,0 +1,19 @@
+void main() {
+  var lastname = 'nadurbekov';
+  String a;
+  a = 'argen';
+  String addres = 'korguz';
+  num ega = 22;
+  num height = 185.0;
+  int weight = 70;
+  double sizeleg = 42.3;
+  bool ismaried = false;
+  print('$lastname');
+  print('$a');
+  print('$addres');
+  print('$ega');
+  print('$height');
+  print("$weight");
+  print('$sizeleg');
+  print('$ismaried');
+}
---
+++ bin/syntax_example.dart
@@ -0,0 +1,13 @@
+void main() {
+  String name = " Argen";
+  String addres = "Korgyzstan";
+  num age = 22;
+  num height = 178;
+  bool isMaried = false;
+
+  print('the String is the $name');
+  print(" the addres is the $addres");
+  print('the age is the $age');
+  print('the height is the $height');
+  print('the is mareid is $isMaried');
+}
